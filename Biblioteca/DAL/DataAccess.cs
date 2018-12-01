c0ebb56f55da4dae366c1df5470b6db1889c2222
tkwCode <tim.kawa@hotmail.com>
2018-12-01 13:53:27 +0100

    Projeto Final João
    Projeto final

---
+++ Biblioteca/DAL/DataAccess.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Biblioteca
+{
+    public class DataAccess : IDisposable
+    {
+        private readonly SqlConnection conexaoSql;
+
+
+        /// <summary>
+        /// Construtor da classe.
+        /// </summary>
+        public DataAccess()
+        {
+            try
+            {
+                //AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", Application.StartupPath + @"temp\Tim.config");
+                var conexao = ConfigurationManager.ConnectionStrings["QbinConnection"].ConnectionString;
+                conexaoSql = new SqlConnection(conexao);
+                conexaoSql.Open();
+                //MessageBox.Show("Consegui!");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Este método é usado para  executar commandos do sql como : Insert,Update e Delete.
+        /// </summary>
+        /// <param name="sqlQuery">O comando sql a ser executado.</param>
+        public void ExecutarComando(string sqlQuery)
+        {
+            try
+            {
+                var comando = new SqlCommand
+                {
+                    CommandText = sqlQuery,
+                    CommandType = CommandType.Text,
+                    Connection = conexaoSql
+                };
+
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+
+        }
+
+        /// <summary>
+        /// Este método é usado para  executar commandos sql de letura de dados como : Select.
+        /// </summary>
+        /// <param name="sqlQuery">O comando sql a ser executado.</param>
+        /// <returns>Retorna uma DataReader.</returns>
+        public SqlDataReader ExecutarComandoReader(string sqlQuery)
+        {
+            try
+            {
+                var comando = new SqlCommand
+                {
+                    CommandText = sqlQuery,
+                    CommandType = CommandType.Text,
+                    Connection = conexaoSql
+                };
+
+                return comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Este método é usado para  executar commandos sql que retorna apenas um valor como : Select.
+        /// </summary>
+        /// <param name="sqlQuery">O comando sql a ser executado.</param>
+        /// <returns>Retorna um valor de tipo string.</returns>
+        public string ExecutarComandoEscalar(string sqlQuery)
+        {
+            try
+            {
+                var comando = new SqlCommand
+                {
+                    CommandText = sqlQuery,
+                    CommandType = CommandType.Text,
+                    Connection = conexaoSql
+                };
+
+                var valor = comando.ExecuteScalar();
+
+                return valor.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Este método é usado para  executar commandos sql que retorna uma DataTable como : Select, select...Inner join.
+        /// </summary>
+        /// <param name="sqlQuery">O comando sql a ser executado.</param>
+        /// <returns>Retorna uma DataTable.</returns>
+        public DataTable ExecutarComandoAdapter(string sqlQuery)
+        {
+            try
+            {
+                var comando = new SqlCommand(sqlQuery, conexaoSql);
+                var adaptador = new SqlDataAdapter(comando);
+                var dados = new DataTable();
+
+                adaptador.Fill(dados);
+
+                return dados;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Metodo que finaliza a conexão depois de fechar a classe.
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                if (conexaoSql.State == ConnectionState.Open)
+                    conexaoSql.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Biblioteca/Dominio/DadosInscricao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class DadosInscricao
+    {
+        public string Nome_Estudante { get; set; }
+        public string Nome_Pai { get; set; }
+        public string Nome_Mae { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Naturalidade { get; set; }
+        public string Provincia { get; set; }
+        public string Municipio { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Num_BI { get; set; }
+        public DateTime Data_Emissao { get; set; }
+        public string Local_Emissão { get; set; }
+
+        [Num_BI] [varchar] (100) NOT NULL,
+ 
+     [Nome_Candidato] [varchar] (150) NOT NULL,
+  
+      [Nome_Pai] [varchar] (150) NOT NULL,
+   
+       [Nome_Mae] [varchar] (150) NOT NULL,
+    
+        [Sexo] [varchar] (15) NOT NULL,
+     
+         [Residencia] [varchar] (150) NOT NULL,
+      
+          [CasaNum] [varchar] (15) NOT NULL,
+       
+           [Bairro] [varchar] (50) NOT NULL,
+        
+            [Data_Nascimento] [date]
+        NOT NULL,
+        
+            [Naturalidade] [varchar] (50) NOT NULL,
+         
+             [Provincia] [varchar] (100) NOT NULL,
+          
+              [Municipio] [varchar] (100) NOT NULL,
+           
+               [Nacionalidade] [varchar] (150) NULL,
+	[Data_EmiBI] [date] NULL,
+	[Local_EmiBI] [varchar] (150) NULL,
+    }
+}
